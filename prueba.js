f892b6420f4234fab0107590691ea0e286a1ab46
Alfredo Arreola <alfredo@sfleet.mx>
2019-09-24 12:49:15 -0500

    my first commit

---
+++ prueba.js
@@ -0,0 +1,5 @@
+"use strict";
+var hola = "hola mundo";
+var sumando = 234;
+var restando = 456;
+var multiplicando = 345;
---
+++ prueba.ts
@@ -0,0 +1,6 @@
+type nuevo = string
+type numero = number
+let hola:nuevo = "hola mundo"
+let  sumando:number = 234
+let restando:numero = 456
+let multiplicando:numero = 345
